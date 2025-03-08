ac2e25a5126702621331197f91ec795f14f1aa13
nechasyifa <nechasyifasyafitri@gmail.com>
2025-03-08 12:22:55 +0700

    Praktikum 2.1 Jobsheet4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,19 +10,21 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $user = UserModel::where('username', 'manager9')->firstOrFail();
-        // return view('user', ['data' => $user]);
-
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
 
     //      // tambah data user dengan Eloquent Model
     //      $data = [
