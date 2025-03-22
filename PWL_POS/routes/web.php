2cacf04fccc3359fa8964eda231d8bbc8903b1e6
nechasyifa <nechasyifasyafitri@gmail.com>
2025-03-22 16:29:08 +0700

    Praktikum 3 Jobsheet5.2

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -26,11 +26,9 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
-
 Route::get('/kategori', [KategoriController::class, 'index']);
 
-Route::get('/user', [UserController::class, 'index']);
-
+// Route::get('/user', [UserController::class, 'index']);
 // Route::get('/', [HomeController::class, 'home']);
 
 Route::prefix('category')->group(function () {
@@ -44,19 +42,10 @@ Route::prefix('category')->group(function () {
 
 Route::get('/penjualan', [PenjualanController::class, 'penjualan']);
 
-//user
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
 Route::get('/kategori/create', [KategoriController::class, 'create']);
 Route::post('/kategori', [KategoriController::class, 'store']);
-
 Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
 Route::put('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-
 Route::post('/kategori/delete/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
 
 //Barang
@@ -68,3 +57,15 @@ Route::get('/barang', [BarangController::class, 'index']);
  Route::post('/barang/delete/{id}', [BarangController::class, 'destroy'])->name('barang.destroy');
 
  Route::get('/', [WelcomeController::class,'index']);
+
+//User
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);           // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);       // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);    // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);          // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);        // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);   // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);      // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
+});
