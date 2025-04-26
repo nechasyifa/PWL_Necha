9bb22923aaac4bcfb3c84d5988f060ddf3f61b6c
nechasyifa <nechasyifasyafitri@gmail.com>
2025-04-26 18:30:48 +0700

    Tugas Implementasi Eloquent Accessor (Barang)

--- PWL_POS/app/Http/Controllers/Api/BarangController.php
+++ PWL_POS/app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -15,13 +16,55 @@ class BarangController extends Controller
 
     public function store(Request $request)
     {
-        $barang = BarangModel::create($request->all());
-        return response()->json($barang, 201);
+        // $barang = BarangModel::create($request->all());
+        // return response()->json($barang, 201);
+
+        // set validation 
+        $validator = Validator::make($request->all(), [
+            'kategori_id' => 'required|exists:m_kategori,kategori_id',
+            'barang_kode' => 'required|unique:m_barang',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        // if validations fails 
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // save image file to storage
+        $image = $request->file('image');
+        $image->storeAs('public/barang', $image->hashName());
+
+        // create barang 
+        $barang = BarangModel::create([
+            'kategori_id' => $request->kategori_id,
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'image' => $image->hashName(),
+        ]);
+
+        // return response JSON barang is created 
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'barang' => $barang,
+            ], 201);
+        }
+
+        // return JSON process insert failed 
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 
     public function show(BarangModel $barang)
     {
-        return BarangModel::find($barang);
+        return response()->json($barang);
     }
 
     public function update(Request $request, BarangModel $barang)
--- PWL_POS/app/Models/BarangModel.php
+++ PWL_POS/app/Models/BarangModel.php
@@ -1,27 +1,48 @@
 <?php
- 
- namespace App\Models;
- 
- use Illuminate\Database\Eloquent\Factories\HasFactory;
- use Illuminate\Database\Eloquent\Model;
- 
- class BarangModel extends Model
- {
-     use HasFactory;
-     protected $table = 'm_barang';
-     protected $primaryKey = 'barang_id';
- 
-     protected $fillable = [
-         'barang_kode',
-         'barang_nama',
-         'harga_beli',
-         'harga_jual',
-         'kategori_id',
-         'stok'
-     ];
- 
-     public function kategori()
-     {
-         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
-     }
- }
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class BarangModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_barang';
+    protected $primaryKey = 'barang_id';
+    // protected $fillable = [
+    //     'barang_id',
+    //     'kategori_id',
+    //     'barang_kode',
+    //     'barang_nama',
+    //     'harga_beli',
+    //     'harga_jual',
+    // ];
+
+    // tugas jobsheet 11
+    protected $fillable = [
+        'kategori_id',
+        'barang_kode',
+        'barang_nama',
+        'harga_beli',
+        'harga_jual',
+        'image',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function kategori(): BelongsTo
+    {
+        return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    // tugas jobsheet 11
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/barang/' . $image),
+        );
+    }
+}
