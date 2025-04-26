da28e6d27c4903301c219fea761e862e25c8c8e4
nechasyifa <nechasyifasyafitri@gmail.com>
2025-04-26 15:46:28 +0700

    Praktikum 2 Jobsheet10

--- PWL_POS/routes/api.php
+++ PWL_POS/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
